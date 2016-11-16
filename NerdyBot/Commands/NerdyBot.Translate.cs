9e49fa19898802d67bd341fbec56b026970e94f2
karakTaka <karakTaka@ZAK>
2016-11-16 15:08:25 +0100

    added translate plugin

---
+++ NerdyBot/Commands/NerdyBot.Translate.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdyBot.Commands
+{
+    class TranslateCommand : ICommand
+    {
+    }
+}
